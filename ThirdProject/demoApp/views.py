bc3c388c268d1decddc5869699330fad5934cd65
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-04-11 12:56:23 +0530

    using urls in application level

--- ThirdProject/demoApp/views.py
+++ ThirdProject/demoApp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def fun1(request):
+	return HttpResponse('<h1> Function 01 </h1>')
+	
+def fun2(request):
+	return HttpResponse('<h2> Function 02 </h2>')
+	
+def fun3(request):
+	return HttpResponse('<h1> Function 03 </h1>')
+	
+def fun4(request):
+	return HttpResponse('<h1> Function 04 </h1>')
+	
+def fun5(request):
+	return HttpResponse('<h2> Function 05 </h2>')
