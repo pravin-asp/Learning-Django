3b441d555bafbe0babc8c50cf734a36d062eaa04
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-04-13 16:36:26 +0530

    Template tags

--- TemplateProject/TemplateApp/views.py
+++ TemplateProject/TemplateApp/views.py
@@ -3,6 +3,12 @@ import datetime
 # Create your views here.
 
 def display(request):
+	message = 'Hi'
 	date = datetime.datetime.now()
-	date_dict = {'display_date' : date, 'name' : 'Pravin A S'}
+	hour = int(date.strftime('%H'))
+	if hour < 12:
+		message += ' Good Morning'
+	else:
+		message += ' Good Evening'
+	date_dict = {'display_date' : date, 'name' : 'Pravin A S', 'message' : message}
 	return render(request, 'TemplateApp/abc.html', context = date_dict)
