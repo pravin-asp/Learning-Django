ed852c38af30ba54628526779d6b199c9531039b
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-04-13 16:22:05 +0530

    template tags

--- TemplateProject/TemplateApp/views.py
+++ TemplateProject/TemplateApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+import datetime
 # Create your views here.
 
 def display(request):
-	return render(request, 'TemplateApp/abc.html')
+	date = datetime.datetime.now()
+	date_dict = {'display_date' : date}
+	return render(request, 'TemplateApp/abc.html', context = date_dict)
