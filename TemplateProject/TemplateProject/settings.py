dd65152d4d979278adaef27ff7d3748ce870705e
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-04-13 15:54:15 +0530

    creating and linking the path for the template

--- TemplateProject/TemplateProject/settings.py
+++ TemplateProject/TemplateProject/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR2 = os.path.dirname(os.path.dirname(os.paht.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR2, 'Templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'TemplateProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
