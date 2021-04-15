fe91660be279caa5640a83fc3b98e9299e9e6de1
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-04-15 11:29:21 +0530

    Practicing the template in django

--- PracticeProject/PracticeApp/views.py
+++ PracticeProject/PracticeApp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 # Create your views here.
 
 def WelcomePage(response):
-	message = '''<h1>Welcome to my Practice Project Page</h1>
+	"""message = '''<h1>Welcome to my Practice Project Page</h1>
 				<h4>This is for my reference Django Project</h4>
 				<p>This project is only for my practice. This helps me to understan 
 				the Django project. How it works? These are my learnings. Thank YOu !!!</p>'''
-	return HttpResponse(message)
+	return HttpResponse(message)"""
+	return render(response, 'Template/practice')
--- PracticeProject/PracticeProject/settings.py
+++ PracticeProject/PracticeProject/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR2 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR2, 'PracticeTemplate')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'PracticeProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
