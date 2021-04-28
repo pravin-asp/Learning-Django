3cc649ac61f3c549945300418babbed8301defc4
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-04-28 11:01:46 +0530

    created upto template display

--- PracticeProject/PracticeDB/DBApp/urls.py
+++ PracticeProject/PracticeDB/DBApp/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path
+from DBApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-	path('DBApp/', include('DBApp.urls')),
+    path('DataBAse/', views.PrintDetails),
 ]
